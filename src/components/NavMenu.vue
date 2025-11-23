e0fd048fa19c3f25f0823610185483672c230d8b
Joel Mitchell <joey5533a@gmail.com>
2025-11-23 06:48:51 -0600

    home page is working

---
+++ src/components/NavMenu.vue
@@ -0,0 +1,20 @@
+
+import HomePage from '@/views/HomePage.vue';
+<template>
+  <nav>
+    <RouterLink :to="{name: 'Home'}">Home</RouterLink>
+    <RouterLink :to="{name: 'MealsByName'}">Meals By Name</RouterLink>
+    <RouterLink :to="{name: 'MealsByLetter'}">Meals By Letter</RouterLink>
+    <RouterLink :to="{name: 'MealsByIngredients'}">Meals By Ingredients</RouterLink>
+  </nav>
+</template>
+
+<style lang="postcss" scoped>
+nav {
+  @apply flex justify-center space-x-4;
+
+  & .router-link-active {
+    @apply underline underline-offset-4;
+  }
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import useAPI from '@/composables/useAPI'
+import Meals from '@/components/Meals.vue'
+
+const { api } = useAPI()
+const meals = ref([])
+
+onMounted(async () => {
+
+    for (let index = 0; index < 9; index++) {
+      api.get('random.php').then( ({data}) => meals.value.push(data.meals[0]))
+      
+    }
+  
+})
+
+</script>
+
+<template>
+  <div class="p-8 pb-0 text-orange-500">
+    <h1 class="text-4xl font-bold mb-4"> Random Meals</h1>
+  </div>
+  <Meals :meals="meals"></Meals>
+</template>
