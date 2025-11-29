f3830723db0e0360266a5b48c1b1381f5fedf7fb
Joel Mitchell <joey5533a@gmail.com>
2025-11-29 08:17:53 -0600

    Data store and meals by letter complete

--- src/store/state.js
+++ src/store/state.js
@@ -1,24 +1,6 @@
-import useAPI from "@/composables/useAPI";
-
-const { api } = useAPI()
-
-export function searchMeals({ commit }, keyword) {
-    api.get(`search.php?s=${keyword}`)
-        .then(({ data }) => {
-        commit('setSearchedMeals', data.meals)
-    })
-}
-
-export function searchMealsByLetter({ commit }, letter) {
-    api.get(`search.php?f=${letter}`)
-        .then(({ data }) => {
-        commit('setMealsByLetter', data.meals)
-    })
-}
-
-export function searchMealsByIngredient({ commit }, ingredient) {
-    api.get(`filter.php?i=${ingredient}`)
-        .then(({ data }) => {
-            commit('setMealsByIngredient', data.meals)
-    })
+export default {
+    searchedMeals: [],
+    mealsByLetter: [],
+    mealsByIngredient: [],
+    ingredient: {}
 }
