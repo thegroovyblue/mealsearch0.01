9438aa3d9de987101dc4b94c119e945188a53650
Joel Mitchell <joey5533a@gmail.com>
2025-12-03 14:57:24 -0600

    Fixed for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/meal-search/',
+  base: '/mealsearch0.01/',
   plugins: [vue()],
   resolve: {
     alias: {
